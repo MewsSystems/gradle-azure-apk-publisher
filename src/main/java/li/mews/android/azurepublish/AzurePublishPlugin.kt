3732da92edc1cba7ac1ce7ee7312afa591937776
ookami-kb <ookami.kb@gmail.com>
2019-03-05 16:34:56 +0100

    Bump version

--- src/main/java/li/mews/android/azurepublish/AzurePublishPlugin.kt
+++ src/main/java/li/mews/android/azurepublish/AzurePublishPlugin.kt
@@ -6,8 +6,7 @@ import org.gradle.api.Project
 
 open class AzurePublishPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        val extension =
-            project.extensions.create("azurePublishConfig", AzurePublishExtension::class.java)
+        val extension = project.extensions.create("azurePublishConfig", AzurePublishExtension::class.java)
 
         project.extensions.getByType(AppExtension::class.java).applicationVariants.all {
             val task = project.tasks.create("azurePublish${it.name.capitalize()}", AzurePublishTask::class.java)
