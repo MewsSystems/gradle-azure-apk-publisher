2ebbb18c2292114378cc27c94f382c1e388afc35
ookami-kb <ookami.kb@gmail.com>
2019-03-07 11:29:03 +0100

    :bug: Add check for AppPlugin presence

--- src/main/java/li/mews/android/azurepublish/AzurePublishPlugin.kt
+++ src/main/java/li/mews/android/azurepublish/AzurePublishPlugin.kt
@@ -1,12 +1,15 @@
 package li.mews.android.azurepublish
 
 import com.android.build.gradle.AppExtension
+import com.android.build.gradle.AppPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 open class AzurePublishPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val extension = project.extensions.create("azurePublishConfig", AzurePublishExtension::class.java)
+
+        if (!project.plugins.hasPlugin(AppPlugin::class.java)) return
 
         project.extensions.getByType(AppExtension::class.java).applicationVariants.all {
             val task = project.tasks.create("azurePublish${it.name.capitalize()}", AzurePublishTask::class.java)
